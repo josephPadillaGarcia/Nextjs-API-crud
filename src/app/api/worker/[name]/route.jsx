66e37b3e0d8d66d6d14376221a10292244105db5
josephpg <jlpadillagarcia19@gmail.com>
2024-03-25 01:17:21 -0500

    get data cargo worker in proccess

--- src/app/api/worker/[name]/route.jsx
+++ src/app/api/worker/[name]/route.jsx
@@ -2,14 +2,10 @@ import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 export async function GET(request, {params}) {
-    //const id = parseInt(params.id);
     const name = params.name;
-    //console.log(params);
-    //return new NextResponse(`respuesta`, {status: 200});
 
     try {
         const res = await prisma.worker.findMany({
-            //where: {idworker: id},
             where: {nombre: name},
         });
         if(!res){
--- src/app/colaborador/page.jsx
+++ src/app/colaborador/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, useEffect } from "react";
+import estilo from "../../../public/scss/main.module.scss"
 
 function Colaborador(){
 
@@ -8,12 +9,22 @@ function Colaborador(){
     const [age, setAge] = useState("");
     const [dni, setDni] = useState("");
     const [cargo, setCargo] = useState("");
+    /*const cargoPromotor = "promotor";    
+    const cargoCabeza = "cabeza";
+    const cargoCoordinador = "coordinador";
+    const cargoJefe = "jefe de equipo"*/
+    const cargoworker = "";
     const point = 0;
 
     const [data, setData] = useState([]);
 
     useEffect(() => {
         getDataWorker();
+        //getDataCargoWorker(cargoworker);
+        /*if (cargoworker == "") {
+            const cargoworker = "promotor";
+            getDataCargoWorker(cargoworker);
+        }*/
     }, [])
 
     const getDataWorker = async () =>{
@@ -21,6 +32,28 @@ function Colaborador(){
         const res = await fetch(url, {method: 'GET', cache: 'no-cache'});
         const resultados  = await res.json();
         setData(resultados.data);
+    }
+
+    const getDataCargoWorker = async (cargoworker) =>{
+             
+        console.log("cargo: " + cargoworker);
+        if (data.length > 0) {
+            setData([]);
+        }
+        //console.log("data:"+data);
+        const url = 'http://localhost:3000/api/workercargo/'+cargoworker;
+        const res = await fetch(url, {method: 'GET', cache: 'no-cache'});
+        const resultcargo  = await res.json();
+        setData(resultcargo.data);
+    }
+
+    const getDataPromotoresWorker = async (cargoworker) => {
+        if (cargoworker == "") {
+            const cargoworker = "promotor";
+            getDataCargoWorker(cargoworker);
+            //console.log("cargo: " + cargoworker);
+        }
+        //console.log("cargo: " + cargoworker);
     }
 
     const deleteworker = async (id) =>{        
@@ -53,86 +86,154 @@ function Colaborador(){
         } catch (error) {
             
         }
-        
-        
-
     }
 
     return(
-        <div className="content">
-            <div className="form-content">
-                <h2>Colaboradores</h2>
-                <form method="" action="" onSubmit={submitForm}>
-                    <div className="form-block">
-                        <label>Nombre</label>
-                        <input 
-                            type="text" 
-                            name="name" 
-                            value={name}
-                            onChange={({target}) => setName(target?.value)}
-                        />
-                    </div>
+        <div className="main">
+            <div className={estilo.headerworker}>
 
-                    <div className="form-block">
-                        <label>Apellido</label>
-                        <input 
-                            type="text" 
-                            name="lastname" 
-                            value={lastname}
-                            onChange={({target}) => setLastName(target?.value)}
-                        />
+                <div className={estilo.container}>
+                    <div className={estilo.gridcol}>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>
+                            <div className={estilo.headerworker__logo}>
+                                <img src="/image/morena-isotipo-blanco.png" alt="" />
+                            </div>
+                        </div>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl6}`}></div>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>                        
+                            <div className={estilo.headerworker__boton}>
+                                <a href="#!">Agregar Usuario</a>
+                            </div>
+                        </div>
                     </div>
+                </div>
 
-                    <div className="form-block">
-                        <label>Edad</label>
-                        <input 
-                            type="number" 
-                            name="age"
-                            value={age}
-                            onChange={({target}) => setAge(target?.value)}
-                        />
+            </div>
+
+            <div className={estilo.tipocargoworker}>
+                <div className={estilo.container}>
+                    <div className={estilo.gridcol}>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>                        
+                            <div className={estilo.tipocargoworker__boton}>
+                                <button onClick={() => getDataPromotoresWorker(cargoworker)}>PROMOTORES</button>
+                            </div>
+                        </div>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>                        
+                            <div className={estilo.tipocargoworker__boton}>
+                                <button onClick={() => getDataCabezasWorker(cargoworker)}>CABEZAS</button>
+                            </div>
+                        </div>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>                        
+                            <div className={estilo.tipocargoworker__boton}>
+                                <button onClick={() => getDataCoordinadoresWorker(cargoworker)}>COORDINADORES</button>
+                            </div>
+                        </div>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>                        
+                            <div className={estilo.tipocargoworker__boton}>
+                                <button onClick={() => getDataJefeWorker(cargoworker)}>JEFE DE EQUIPO</button>
+                            </div>
+                        </div>
                     </div>
+                </div>
+            </div>
 
-                    <div className="form-block">
-                        <label>DNI</label>
-                        <input 
-                            type="number" 
-                            name="dni"
-                            value={dni}
-                            onChange={({target}) => setDni(target?.value)}
-                        />
+            <div className="content">
+                <div className="form-content">
+                    <h2>Colaboradores</h2>
+                    <form method="" action="" onSubmit={submitForm}>
+                        <div className="form-block">
+                            <label>Nombre</label>
+                            <input 
+                                type="text" 
+                                name="name" 
+                                value={name}
+                                onChange={({target}) => setName(target?.value)}
+                            />
+                        </div>
+
+                        <div className="form-block">
+                            <label>Apellido</label>
+                            <input 
+                                type="text" 
+                                name="lastname" 
+                                value={lastname}
+                                onChange={({target}) => setLastName(target?.value)}
+                            />
+                        </div>
+
+                        <div className="form-block">
+                            <label>Edad</label>
+                            <input 
+                                type="number" 
+                                name="age"
+                                value={age}
+                                onChange={({target}) => setAge(target?.value)}
+                            />
+                        </div>
+
+                        <div className="form-block">
+                            <label>DNI</label>
+                            <input 
+                                type="number" 
+                                name="dni"
+                                value={dni}
+                                onChange={({target}) => setDni(target?.value)}
+                            />
+                        </div>
+
+                        <div className="form-block">
+                            <label>Cargo</label>
+                            <input 
+                                type="text" 
+                                name="Cargo"
+                                value={cargo}
+                                onChange={({target}) => setCargo(target?.value)}
+                            />
+                        </div>
+
+                        <button type="submit" className="button">Agregar Colaborador</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className={estilo.tableworker}>
+                <div className={estilo.container}>
+                    <div className={estilo.gridcol}>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl9}`}>
+                            <div className="content__worker  content__worker__scroll">
+                                <div className="">
+                                    <div className="worker">
+                                        <p>NOMBRE Y APELLIDO</p>
+                                        <p>PUNTAJE</p>
+                                        <p>ACTION</p>
+                                    </div>
+                                </div>
+                                {
+                                    data.map(w => (
+                                        <div className="worker">
+                                            <p>{w.nombre} {w.apellido}</p>
+                                            <p>{w.points}</p>
+                                            <button className="button" onClick={() => deleteworker(w.idworker)}>Borrar</button>                    
+                                        </div>
+                                    ))
+                                }                                 
+                            </div>
+                        </div>
+                        <div className={`${estilo.grids12} ${estilo.gridm6} ${estilo.gridl3}`}>
+                            <div className={estilo.tipocargoworker__content}>
+                                <div className={estilo.tipocargoworker__boton}>
+                                    <button onClick={() => deleteworker(w.idworker)}>ELIMINAR PUNTAJE</button>
+                                </div>
+                                <div className={estilo.tipocargoworker__img}>
+                                    <img src="/image/editado.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div className="form-block">
-                        <label>Cargo</label>
-                        <input 
-                            type="text" 
-                            name="Cargo"
-                            value={cargo}
-                            onChange={({target}) => setCargo(target?.value)}
-                        />
-                    </div>
-
-                    <button type="submit" className="button">Agregar Colaborador</button>
-                </form>
+                </div>
             </div>
 
 
-            <h2>Lista de Trabajadores</h2>
-                <div className="content__worker  content__worker__scroll">
-                    {
-                        data.map(w => (
-                            <div className="worker">
-                                <p>{w.nombre}</p>
-                                <p>{w.apellido}</p>
-                                <p>{w.edad}</p>
-                                <p>{w.dni}</p>
-                                <p>{w.points}</p>
-                                <button className="button" onClick={() => deleteworker(w.idworker)}>Borrar</button>                    
-                            </div>
-                        ))
-                    }                                 
-                </div>
         </div>
     )
 }
